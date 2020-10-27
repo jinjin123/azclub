4f4fec04a253f94f491c21453a54497c4fa45385
Jinjin123 <1293813551@qq.com>
2020-10-27 18:29:24 +0800

    modidy step form two

--- web/modules/custom/azhealthclub_step_login/src/Form/MultistepTwoForm.php
+++ web/modules/custom/azhealthclub_step_login/src/Form/MultistepTwoForm.php
@@ -27,6 +27,9 @@ class MultistepTwoForm extends MultistepFormBase {
 
     $form = parent::buildForm($form, $form_state);
 
+    $entityFieldManager = \Drupal::service('entity_field.manager');
+    $memberFields = $entityFieldManager->getFieldDefinitions('profile', 'member');
+
     $form['zh_name'] = [
       '#type' => 'textfield',
       '#title' => '中文名字*',
@@ -55,40 +58,31 @@ class MultistepTwoForm extends MultistepFormBase {
       '#date_timezone' => 'Asia/Kolkata',
     ];
 
+    $field_attention1_settings = $memberFields['field_gender']->getSettings();
+    $gender_allowed_values = $field_attention1_settings['allowed_values'];
     $form['gender'] = [
       '#type' => 'select',
       '#title' => '性別*',
-      '#options' => [
-        'male' => '男',
-        'female' => '女',
-      ],
+      '#options' => $gender_allowed_values,
       '#default_value' => $this->store->get('gender') ? $this->store->get('gender') : '',
     ];
 
-    $form['field_attention']['member_attention1'] = [
-      '#type' => 'checkbox',
-      '#title' => '本人願意接收有關阿斯利康健康講座/活動、藥物產品，會員優惠發健康醫學等資訊，並同意可為該目的使用本人個人資料。',
-      '#default_value' => $this->store->get('member_attention1') ? $this->store->get('member_attention1') : '',
+    $field_attention1_settings = $memberFields['field_attention2']->getSettings();
+    $field_attention2_allowed_values = $field_attention1_settings['allowed_values'];
+    $form['field_attention2'] = [
+      '#type' => 'checkboxes',
+      '#options' => $field_attention2_allowed_values,
+      '#default_value' => $this->store->get('field_attention2') ? $this->store->get('field_attention2') : [],
     ];
 
+    $field_attention1_settings = $memberFields['field_communication_mode']->getSettings();
+    $allowed_values = $field_attention1_settings['allowed_values'];
     $form['communication_mode'] = [
-      '#markup' => '如閣下同意我們使用您的個人資料，透過以下方式通知您關於本公司會員優惠/活動、藥物產品及健康資訊，請於以下空格內打鉤「」：
-（如沒有填寫，我們沒法通知閣下有關會員優惠/活動發資訊）',
-    ];
-    $form['communication_mode']['phone']= [
-      '#type' => 'checkbox',
-      '#title' => '電話',
-      '#default_value' => $this->store->get('communication_mode.phone') ? $this->store->get('communication_mode.phone') : '',
-    ];
-    $form['communication_mode']['phone_text']= [
-      '#type' => 'checkbox',
-      '#title' => '電話短訊',
-      '#default_value' => $this->store->get('communication_mode.phone_text') ? $this->store->get('communication_mode.phone_text') : '',
-    ];
-    $form['communication_mode']['email']= [
-      '#type' => 'checkbox',
-      '#title' => '電子郵件',
-      '#default_value' => $this->store->get('communication_mode.email') ? $this->store->get('communication_mode.email') : '',
+      '#type' => 'checkboxes',
+      '#title' => '如閣下同意我們使用您的個人資料，透過以下方式通知您關於本公司會員優惠/活動、藥物產品及健康資訊，請於以下空格內打鉤「」：
+      （如沒有填寫，我們沒法通知閣下有關會員優惠/活動發資訊）',
+      '#options' => $allowed_values,
+      '#default_value' => $this->store->get('communication_mode') ? $this->store->get('communication_mode') : [],
     ];
 
     $form['actions']['previous'] = [
@@ -114,10 +108,8 @@ class MultistepTwoForm extends MultistepFormBase {
     $this->store->set('en_name', $form_state->getValue('en_name'));
     $this->store->set('identification_last4num', $form_state->getValue('identification_last4num'));
     $this->store->set('birthday', $form_state->getValue('birthday'));
-    $this->store->set('member_attention1', $form_state->getValue('member_attention1'));
-    $this->store->set('communication_mode.phone', $form_state->getValue('phone'));
-    $this->store->set('communication_mode.phone_text', $form_state->getValue('phone_text'));
-    $this->store->set('communication_mode.email', $form_state->getValue('email'));
+    $this->store->set('field_attention2', $form_state->getValue('field_attention2'));
+    $this->store->set('communication_mode', $form_state->getValue('communication_mode'));
 
     $form_state->setRedirect('azhealthclub_step_login.multistep_three');
   }
