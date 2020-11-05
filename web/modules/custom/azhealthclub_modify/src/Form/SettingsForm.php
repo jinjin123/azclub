90c9e68804af214b5033448152f74cbfffe70bf2
Jinjin123 <1293813551@qq.com>
2020-11-05 14:45:24 +0800

    makes user can choose therapeutic area content types

--- web/modules/custom/azhealthclub_modify/src/Form/SettingsForm.php
+++ web/modules/custom/azhealthclub_modify/src/Form/SettingsForm.php
@@ -13,7 +13,7 @@ class SettingsForm extends ConfigFormBase {
   * {@inheritdoc}
   */
   public function getFormId() {
-    return 'azhealthclub_modify_settings';
+    return 'azhealthclub_modify_settings_form';
   }
 
   /**
@@ -30,12 +30,18 @@ class SettingsForm extends ConfigFormBase {
   */
   public function buildForm(array $form, FormStateInterface $form_state) {
     $config = $this->config('azhealthclub_modify.settings');
+    $contentTypes = \Drupal::entityTypeManager()->getStorage('node_type')->loadMultiple();
+    $contentTypesList = [];
+    foreach ($contentTypes as $contentType) {
+      $contentTypesList[$contentType->id()] = $contentType->label();
+    }
 
     $form['ta_content_types'] = array(
-      '#type' => 'textarea',
-      '#title' => $this->t('TA content types'),
-      '#description' => $this->t('The TA content types'),
-      '#default_value' => $config->get('ta_content_types'),
+      '#type' => 'checkboxes',
+      '#options' => $contentTypesList,
+      '#title' => $this->t('Choose all therapeutic area content types'),
+      '#description' => '',
+      '#default_value' => $config->get('ta_content_types') ? : [],
     );
 
     return parent::buildForm($form, $form_state);
@@ -48,7 +54,7 @@ class SettingsForm extends ConfigFormBase {
     // Retrieve the configuration
     $this->configFactory->getEditable('azhealthclub_modify.settings')
     // Set the submitted configuration setting
-    ->set('ta_content_types', $form_state->getValue('ta_content_types'))
+    ->set('ta_content_types', array_filter($form_state->getValue('ta_content_types')))
     ->save();
 
     parent::submitForm($form, $form_state);
